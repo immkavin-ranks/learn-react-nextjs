9708a26048426cdad5b6aeaf449b2dac220bdf0b
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-06-08 16:50:54 +0530

    index.html transforms to page.js

---
+++ app/page.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+function Header({ title }) {
+  return(<h1>{title ? `React.JS - ${title}` : 'Default Title'}</h1>);
+}
+
+export default function HomePage() {
+  const names = ['Kavin', 'Jayashri', 'Manoharan', 'Maheswari'];
+  const [likes, setLikes] = React.useState(0);
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+  return(
+    <div>
+      <Header title='Develop. Preview. Ship.' />
+      <ul>
+        {names.map((name) => <li key={name}>{name}</li>)}
+      </ul>
+      <button onClick={handleClick}>Like({likes})</button>
+    </div>
+  )
+}
